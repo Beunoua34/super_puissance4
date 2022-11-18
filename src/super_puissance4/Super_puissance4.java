39ff5eee7feb936a5e878b25cec66498e7a49757
BENOIT  BASSOMPIERRE <benoit.bassompierre@epfedu.fr>
2022-11-18 10:27:07 +0100

    test des commit avec guilhem

--- src/super_puissance4/Super_puissance4.java
+++ src/super_puissance4/Super_puissance4.java
@@ -14,7 +14,7 @@ public class Super_puissance4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //on teste le bail
     }
     
 }
